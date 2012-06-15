fb49c7199d9bb26c157293be5f258e721ef3ddd6
jwcarroll <jwarren.carroll@gmail.com>
2012-06-15 18:40:39 -0400

    Manually parsing out the Raven Connection String

--- KanBan.Demo/Data/DocStore.cs
+++ KanBan.Demo/Data/DocStore.cs
@@ -11,19 +11,28 @@ namespace KanBan.Demo.Data
 {
     public static class DocStore
     {
+        private const Int32 URL = 0;
+        private const Int32 API_KEY = 1;
+
         static DocStore()
         {
-            Current = new DocumentStore { Url = GetUrlFromSettings() };
+            Current = new DocumentStore {
+                Url = GetConnectionStringPart("Url=", URL),
+                ApiKey = GetConnectionStringPart("ApiKey=", URL)
+            };
+
             Current.Initialize();
      
             IndexCreation.CreateIndexes(Assembly.GetCallingAssembly(), Current);
         }
 
-        private static string GetUrlFromSettings()
+        //Wonky little hack to handle AppHarbor
+        private static string GetConnectionStringPart(String prefix, Int32 index)
         {
             var connectionString = ConfigurationManager.AppSettings["RAVENHQ_CONNECTION_STRING"];
+            var parts = connectionString.Split(';');
 
-            return connectionString.Replace("Url=", String.Empty);
+            return parts[index].Replace(prefix, String.Empty);
         }
 
         public static DocumentStore Current { get; private set; }
